8d978afa7dff120d1bcd4b309ec6bae2bfe6ffd2
Fanjin Zhang <zfjsail@163.com>
2018-06-22 00:00:20 +0800

    cubic spline

--- algo.py
+++ algo.py
@@ -1,4 +1,26 @@
 import numpy as np
+from scipy.linalg import solve_banded
+
+
+def binary_search_interval(x, start, end, intervals=20): # assume start <= x <= end
+    range_end = intervals + 1
+    h = (end - start) / intervals
+    inter_points = [start + h * i for i in range(range_end)]
+    li = 0
+    ri = intervals
+    while ri - li > 1:
+        mid = (ri + li) // 2
+        mid_value = inter_points[mid]
+        # print(mid, mid_value)
+        # print('-----')
+        # print(li, ri)
+        if mid_value == x:
+            return mid - 1 # return left
+        elif mid_value < x:
+            li = mid
+        else:
+            ri = mid
+    return li
 
 
 class Interpolation:
@@ -64,3 +86,31 @@ class Interpolation:
                     break
             return y
         return f_piecewise
+
+    def spline(self, start, end, intervals=20):
+        range_end = intervals + 1
+        h = (end - start) / intervals
+        inter_points = [start + h * i for i in range(range_end)]
+
+        m = np.zeros(intervals+1)
+        a = np.zeros((3, intervals-1))
+        a[0, 1:-1] = 0.5
+        a[1, :] = 2
+        a[2, 1:-1] = 0.5
+        d1 = np.zeros(intervals)
+        for i in range(len(d1)):
+            d1[i] = (self.func(inter_points[i+1]) - self.func(inter_points[i]))/h
+        d2 = np.zeros(intervals-1)
+        for i in range(len(d2)):
+            d2[i] = 6*(d1[i+1]-d1[i])/(2*h)
+        m[1:-1] = solve_banded((1, 1), a, d2)
+
+        def f_spline(x):
+            li = binary_search_interval(x, start, end, intervals)
+            x_left = inter_points[li]
+            x_right = inter_points[li+1]
+            print(li, m[li]/(6*h), (self.func(x_left)-(m[li]*h*h)/6)/h)
+            s = m[li]*((x_right-x)**3)/(6*h) + m[li+1]*((x-x_left)**3)/(6*h) + \
+                (self.func(x_left)-(m[li]*h*h)/6)*(x_right-x)/h + (self.func(x_right) - m[li+1]*h*h/6)*(x-x_left)/h
+            return s
+        return f_spline
