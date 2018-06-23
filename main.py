e620133aa0b8204217766fc72578778cb905dfa3
Fanjin Zhang <zfjsail@163.com>
2018-06-23 12:02:09 +0800

    add func2, refactor and plot discontinuous func

--- main.py
+++ main.py
@@ -7,17 +7,41 @@ def runge(x):
     return 1 / (1 + 25*x**2)
 
 
+def f2(x):
+    if -1 <= x < 0:
+        return np.sin(np.pi*x)
+    elif 0 <= x < 0.5:
+        return np.cos(np.pi*x)
+    elif 0.5 <= x <= 1:
+        return 0
+
+
+app_f = f2
+prefix = 'f2'
+
+
 if __name__ == '__main__':
     start = -1
     end = 1
-    itp = Interpolation(runge)
-    # f_newton = itp.newton(start, end)
-    # vis_func(runge, start, end, 'runge')
-    # vis_func(f_newton, start, end, 'runge_newton')
-    # lang_xrange = [np.cos((2*i+1)/42*np.pi) for i in range(21)]
-    # f_lang = itp.lagrange(lang_xrange)
-    # vis_func(f_lang, start, end, 'langrange')
-    # f_piece_linear = itp.piecewise_linear(start, end)
-    # vis_func(f_piece_linear, start, end, 'piecewise_linear')
-    f_spline = itp.spline(start, end)
-    vis_func(f_spline, start, end, 'spline')
+    funcs = [runge, f2]
+    f_names = ['runge', 'f2']
+    methods = ['newton', 'langrange', 'piecewise_linear', 'spline']
+    for i in range(len(funcs)):
+        cur_f = funcs[i]
+        cur_fname = f_names[i]
+        itp = Interpolation(cur_f)
+        vis_func(cur_f, start, end, cur_fname, cont=False)
+        for j in range(len(methods)):
+            cur_method = methods[j]
+            print(cur_fname, cur_method)
+            f_itp = None
+            if cur_method == 'newton':
+                f_itp = itp.newton(start, end)
+            elif cur_method == 'langrange':
+                lang_xrange = [np.cos((2*i+1)/42*np.pi) for i in range(21)]
+                f_itp = itp.lagrange(lang_xrange)
+            elif cur_method == 'piecewise_linear':
+                f_itp = itp.piecewise_linear(start, end)
+            elif cur_method == 'spline':
+                f_itp =itp.spline(start, end)
+            vis_func(f_itp, start, end, '{}_{}'.format(cur_fname, cur_method))
--- vis.py
+++ vis.py
@@ -3,10 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def vis_func(f, start, end, name, intervals=1000):
+def vis_func(f, start, end, name, cont=True, intervals=1000):
     x_range = np.linspace(start, end, intervals)
-    y_range = [f(x) for x in x_range]
+    y_range = np.array([f(x) for x in x_range])
     # y_range = f(x_range)
+    if not cont:
+        pos = np.where(np.abs(np.diff(y_range)) >= 0.5)[0]  # tackle discontinuous point
+        x_range[pos] = np.nan
+        y_range[pos] = np.nan
     plt.plot(x_range, y_range)
     plt.title(name)
     out_dir = 'out'
